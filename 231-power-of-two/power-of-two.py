7b2ac1c44e4921a1e3641de2cf28f44f88d695b8
Harsh020802 <92252627+Harsh020802@users.noreply.github.com>
2024-02-19 22:05:43 +0530

    Time: 13 ms (76.54%) | Memory: 11.6 MB (86.92%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        return (n % 2 == 0) and self.isPowerOfTwo(n // 2)
+        
